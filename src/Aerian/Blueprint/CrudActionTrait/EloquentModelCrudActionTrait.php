cd80afb683ea603e723d0fd11ca3272dc7274393
Simon Neaves <simon@aerian.com>
2017-02-22 12:24:28 +0000

    Create Crud Action traits to support serving APIs for react CRUD app

---
+++ src/Aerian/Blueprint/CrudActionTrait/EloquentModelCrudActionTrait.php
@@ -0,0 +1,90 @@
+<?php
+namespace Aerian\Blueprint\CrudActionTrait;
+
+/**
+ * Trait CrudActionTrait
+ * This trait provides functionality for CRUD controllers including implementing default actions
+ */
+trait EloquentModelCrudActionTrait
+{
+    /**
+     * @var
+     * @todo currently assumes an Aerian\Database\Eloquent\Model, but should be decoupled?
+     */
+    protected $_model;
+
+    /**
+     *
+     * @var string
+     */
+    protected $_modelBasePath = 'App\\';
+
+    /**
+     * @param $entityType string e.g. 'product' or 'productCategory'
+     * @return $this
+     */
+    protected function setModel($entityType)
+    {
+        $entityClass = $this->getEntityClass($entityType);
+        $this->_model = new $entityClass;
+        return $this;
+    }
+
+    protected function getEntityClass($entityType)
+    {
+        return $this->_modelBasePath . ucfirst($entityType);
+    }
+
+    public function index($entity, $limit = 5, $offset =0)
+    {
+        $this->setModel($entity);
+        return $this->_getListArray($limit, $offset);
+    }
+
+    public function blueprint($entity, $id = null)
+    {
+        $this->setModel($entity);
+
+        return $this->_getValidRowOr404($id)
+            ->blueprint()
+            ->toNormalizedArray();
+    }
+
+    protected function _getListArray($limit, $offset)
+    {
+        $selectColumns = $this->_model->getListColumns();
+        $key = $this->_model->getKeyName();
+        array_unshift($selectColumns, $key);
+
+        $rows = $this->_model->limit($limit)->offset($offset)->get($selectColumns)->keyBy($key);
+        $listColumns = $this->_model->getListColumns();
+
+        return [
+            'columnsIds' => array_keys($listColumns),
+            'columns' => $listColumns,
+            'itemIds' => $rows->pluck($key),
+            'items' => $rows
+        ];
+    }
+
+    /**
+     * @param $id
+     * @return \Aerian\Database\Eloquent\Model
+     * @todo this is currenctly coupled to Aerian\Database\Eloquent\Model, but should support other types of models
+     */
+    protected function _getValidRowOr404($id)
+    {
+        if ($id) {
+            $row = $this->_model->find($id);
+        } else {
+            $row = $this->_model;
+        }
+
+        if (!$row instanceof \Aerian\Database\Eloquent\Model) {
+            abort(404);
+        }
+
+        return $row;
+    }
+
+}
---
+++ src/Aerian/Blueprint/CrudActionTrait/ServiceModelCrudActionTrait.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace Aerian\Blueprint\CrudActionTrait;
+
+use Illuminate\Support\Facades\App;
+use Aerian\Blueprint\Adaptor\ServiceModel as ServiceModelAdaptor;
+use Aerian\Blueprint\Adaptor\ServiceModelRecord as ServiceModelRecordAdaptor;
+
+trait ServiceModelCrudActionTrait
+{
+    protected $_model;
+
+    /**
+     * @param string $entityName
+     * @param int $limit
+     * @param int $offset
+     * @return array
+     */
+    public function index($entityName, $limit = 50, $offset = 0)
+    {
+        $this->_setModelByEntityName($entityName);
+
+        $collection = $this->_model->getCollection(['limit' => $limit, 'offset' => $offset]);
+
+        $itemIds = $items = []; //declare itemIds and items as empty arrays
+        $columns = $this->_model->getListColumns();
+
+        //populate the itemIds and items arrays
+        foreach ($collection as $item) {
+            $itemIds[] = $item->id;
+            $items[$item->id] = array_intersect_key($item->toArray(), array_flip($columns)); //only include the columns specified in the columns array
+        }
+
+        return [
+            'columns' => $columns,
+            'itemIds' => $itemIds,
+            'items' => $items
+        ];
+    }
+
+    public function blueprint($entityName, $id = null)
+    {
+        $this->_setModelByEntityName($entityName);
+
+        if ($id) {
+            $record = $this->_model->getPlugin($id);
+            if ($record) {
+                $adaptor = new ServiceModelRecordAdaptor();
+                $model = $record;
+            } else {
+                $adaptor = null;
+                $model = null;
+                abort(404);
+            }
+        } else {
+            $adaptor = new ServiceModelAdaptor();
+            $model = $this->_model;
+        }
+
+        return $adaptor->blueprint($model)->toNormalizedArray();
+
+    }
+
+    public function save($entityName, $id)
+    {
+        $this->_setModelByEntityName($entityName);
+
+        $record = $this->_model->putPlugin($id, request()->all());
+
+        return (new ServiceModelRecordAdaptor())->blueprint($record)->toNormalizedArray();
+    }
+
+    /**
+     * takes an entityName e.g. 'product-category' and sets a model based on a facade accessor e.g. ProductCategoryModel
+     * @param $entityName
+     * @return $this
+     */
+    protected function _setModelByEntityName($entityName)
+    {
+        $this->_setModelByFacadeAccessor($this->_getFacadeAccessorFromEntityName($entityName));
+        return $this;
+    }
+
+    /**
+     * Based on an entity name (e.g. 'product-category') sets an instance of an Aerian/ServiceModel\Model to the
+     * ::_model property of this object
+     * @param string $facadeAccessorString
+     * @return $this
+     */
+    protected function _setModelByFacadeAccessor($facadeAccessorString)
+    {
+        $this->_model = App::make($facadeAccessorString);
+        return $this;
+    }
+
+    /**
+     * returns the facade accessor string (used to reference a model) from a entity name.
+     * e.g. coverts 'product-category' to 'ProductCategoryModel'. In this example, there should be a facade
+     * for retrieving an instance of ProductCategoryModel as per laravel
+     * @see https://laravel.com/docs/5.4/facades
+     * @param string $entityName
+     * @return string
+     */
+    protected function _getFacadeAccessorFromEntityName($entityName)
+    {
+        return studly_case($entityName) . 'Model';
+    }
+}
